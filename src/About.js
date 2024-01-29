12373a3ee91417fdf956f05ed6c993c1489e5d72
jhuni shukla <jhuni3040@gmail.com>
2024-01-29 21:25:39 +0530

    react

---
+++ src/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+function Myabout()
+{
+  return <h1>This is default export and import</h1>
+}
+export default Myabout;
+
+
+export function Myabout1()
+{
+  return <h1>This is name export and import</h1>
+}
+
+export function Myabout2()
+{
+  return <h1>This is name export and import</h1>
+}
+
+export function Myabout3()
+{
+  return <div>
+  <h1>This is name export and import</h1>
+  <p>Lorem ipsum dolor sit amet.</p>
+  <ul>
+    <li>Home</li>
+    <li>Home</li>
+    <li>Home</li>
+    <li>Home</li>
+  </ul>
+  </div>
+}
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Myabout from './About';
+ import {Myabout1,Myabout2,Myabout3} from './About';
+
+
+const root= ReactDOM.createRoot(document.getElementById('root')) ; 
+root.render(
+     <React.StrictMode>
+ <Myabout></Myabout>
+ <Myabout1></Myabout1>
+ <Myabout2></Myabout2>
+ <Myabout3></Myabout3>
+ </React.StrictMode>
+ );
+
+
+
+
+
+
